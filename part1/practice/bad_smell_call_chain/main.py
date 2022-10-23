c480b9b5b9799ffea370901dedea567f8f8834e5
Zidolch <yury.batin@gmail.com>
2022-10-24 00:51:15 +0300

    hw

--- part1/practice/bad_smell_call_chain/main.py
+++ part1/practice/bad_smell_call_chain/main.py
@@ -31,15 +31,20 @@ class Planet:
 
 
 class Person:
-    def __init__(self):
-        self.planet = Planet()
+    def __init__(self, room_number, city_pop):
+        self.room_number = room_number
+        self.city_pop = city_pop
 
     def get_person_room(self):
-        return self.planet.get_contry().get_city().get_street().get_room().get_name()
+        return self.room_number
 
     def get_city_population(self):
-        return self.planet.get_contry().get_city().population()
+        return self.city_pop
 
 
-# TODO после выполнения задания попробуйте
-# сделать экземпляр класса person и вызвать новые методы.
+if __name__ == '__main__':
+    city = City()
+    room = Room()
+    person = Person(room.get_name(), city.population())
+    print(person.get_person_room())
+    print(person.get_city_population())
--- part1/practice/bad_smell_long_parameter_list/main.py
+++ part1/practice/bad_smell_long_parameter_list/main.py
@@ -11,40 +11,32 @@
 
 
 class Unit:
-    def move(self, field, x_coord, y_coord, direction, is_fly, crawl, speed = 1):
+    def __init__(self, field, state, x_coord, y_coord, speed=1):
+        self.field = field
+        self.state = state
+        self.x = x_coord
+        self.y = y_coord
+        self.speed = speed
 
-        if is_fly and crawl:
-            raise ValueError('Рожденный ползать летать не должен!')
+    def _get_speed(self):
+        if self.state == 'fly':
+            return self.speed * 1.2
+        elif self.state == 'crawl':
+            return self.speed * 0.5
+        else:
+            raise ValueError('Неизвестное состояние')
 
-        if is_fly:
-            speed *= 1.2
-            if direction == 'UP':
-                new_y = y_coord + speed
-                new_x = x_coord
-            elif direction == 'DOWN':
-                new_y = y_coord - speed
-                new_x = x_coord
-            elif direction == 'LEFT':
-                new_y = y_coord
-                new_x = x_coord - speed
-            elif direction == 'RIGTH':
-                new_y = y_coord
-                new_x = x_coord + speed
-        if crawl:
-            speed *= 0.5
-            if direction == 'UP':
-                new_y = y_coord + speed
-                new_x = x_coord
-            elif direction == 'DOWN':
-                new_y = y_coord - speed
-                new_x = x_coord
-            elif direction == 'LEFT':
-                new_y = y_coord
-                new_x = x_coord - speed
-            elif direction == 'RIGTH':
-                new_y = y_coord
-                new_x = x_coord + speed
+    def move(self, direction):
 
-            field.set_unit(x=new_x, y=new_y, unit=self)
+        speed = self._get_speed()
+        if direction == 'UP':
+            self.field.set_unit(x=self.x, y=self.y + speed, unit=self)
+        elif direction == 'DOWN':
+            self.field.set_unit(x=self.x, y=self.y - speed, unit=self)
+        elif direction == 'LEFT':
+            self.field.set_unit(x=self.x - speed, y=self.y, unit=self)
+        elif direction == 'RIGTH':
+            self.field.set_unit(x=self.x + speed, y=self.y, unit=self)
+
 
 #     ...
